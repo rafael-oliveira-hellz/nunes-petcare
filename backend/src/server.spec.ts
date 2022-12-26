850fe27a3c0e1e90534150e8aa7395d64a176f3a
Rafael de Oliveira (Hellz) <rafael.oliveirasylva@gmail.com>
2022-12-25 23:19:53 -0300

    chore: setup prettier and start creating project structure

--- backend/src/server.spec.ts
+++
@@ -1,7 +0,0 @@
-import sayHelloWorld from "@/server";
-
-describe("sayHelloWorld", () => {
-  it("should return the message", () => {
-    expect(sayHelloWorld("Hello World")).toBe("Hello World");
-  });
-});
