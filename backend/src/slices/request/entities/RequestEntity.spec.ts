47967046a18ac1ffec885931f9d28f3d2c8e10e0
Rafael de Oliveira (Hellz) <rafael.oliveirasylva@gmail.com>
2022-12-27 01:13:38 -0300

    chore: implements Request CRUD and unit tests

--- backend/src/slices/request/entities/RequestEntity.spec.ts
+++ backend/src/slices/request/entities/RequestEntity.spec.ts
@@ -1,13 +1,41 @@
+import { fidelityEntityMock } from "@/slices/fidelity/entities/FidelityEntity.spec";
+import { orderEntityMock } from "@/slices/order/entities/OrderEntity.spec";
+import { recurrenceEntityMock } from "@/slices/recurrence/entities/RecurrenceEntity.spec";
+import { rideEntityMock } from "@/slices/ride/entities/RideEntity.spec";
 import MockDate from "mockdate";
 import { RequestEntity } from "./RequestEntity";
 
 export const requestEntityMock = {
-    _id: "5f7b5f9b0b9b9b0b9b0b9b0b",
-    createdById: "5f7b5f9b0b9b9b0b9b0b9b0b",
-    name: "Request name",
+    _id: "123",
+    createdById: "123",
+    name: "fakeRequestEntity",
     active: true,
     createdAt: new Date(),
     updatedAt: new Date(),
+    hasDelivery: true,
+    hasRecurrence: true,
+    hasFidelity: true,
+    hasRide: true,
+    message: "Olá fulano, gostaria de marcar horário as 10h da manhã",
+    serviceId: "fakeServiceId",
+    createdForId: "fakeUserId",
+    ownerId: "fakeUserId",
+    customerId: "fakeUserId",
+    customerUserId: "fakeUserId",
+    professionalId: "fakeUser2Id",
+    status: 1,
+    initDate: new Date().toISOString(),
+    endDate: new Date().toISOString(),
+    read: false,
+    push: false,
+    email: false,
+    cancelledAt: null,
+    order: orderEntityMock,
+    fidelity: fidelityEntityMock,
+    recurrence: recurrenceEntityMock,
+    ride: rideEntityMock,
+    updatedById: "61c1f9d0e399d2917bdff44e",
+    updatedByRole: "admin",
 };
 
 export const requestEntityPaginatedMock = {
@@ -49,6 +77,9 @@ describe("RequestEntity", () => {
             active: false,
             createdAt: new Date(),
             updatedAt: new Date(),
+            status: 0,
+            updatedById: null,
+            updatedByRole: null,
         });
     });
 });
--- backend/src/slices/request/entities/RequestEntity.ts
+++ backend/src/slices/request/entities/RequestEntity.ts
@@ -1,3 +1,8 @@
+import { FidelityData } from "@/slices/fidelity/entities";
+import { OrderData } from "@/slices/order/entities";
+import { RecurrenceData } from "@/slices/recurrence/entities";
+import { RideData } from "@/slices/ride/entities";
+
 export type RequestData = {
     _id?: string;
     createdById: string;
@@ -5,6 +10,30 @@ export type RequestData = {
     active?: boolean;
     createdAt?: Date;
     updatedAt?: Date;
+    message: string;
+    serviceId: string;
+    ownerId: string;
+    customerId: string;
+    customerUserId?: string;
+    professionalId: string;
+    status: number;
+    createdForId: string;
+    updatedById?: string | null;
+    updatedByRole?: string | null;
+    read: boolean;
+    push: boolean;
+    email: boolean;
+    hasDelivery?: boolean;
+    hasRecurrence?: boolean;
+    hasFidelity?: boolean;
+    hasRide?: boolean;
+    fidelity?: FidelityData;
+    ride?: RideData;
+    recurrence?: RecurrenceData;
+    order?: OrderData;
+    initDate: string;
+    endDate: string;
+    cancelledAt?: Date | null;
 };
 
 export type RequestPaginatedData = {
@@ -18,6 +47,30 @@ export class RequestEntity {
     active?: boolean;
     createdAt?: Date;
     updatedAt?: Date;
+    message: string;
+    serviceId: string;
+    ownerId: string;
+    customerId: string;
+    customerUserId?: string;
+    professionalId: string;
+    status: number;
+    createdForId: string;
+    updatedById?: string | null;
+    updatedByRole?: string | null;
+    read: boolean;
+    push: boolean;
+    email: boolean;
+    hasDelivery?: boolean;
+    hasRecurrence?: boolean;
+    hasFidelity?: boolean;
+    hasRide?: boolean;
+    fidelity?: FidelityData;
+    ride?: RideData;
+    recurrence?: RecurrenceData;
+    order?: OrderData;
+    initDate: string;
+    endDate: string;
+    cancelledAt?: Date | null;
 
     constructor(data: RequestData) {
         this.createdById = data.createdById;
@@ -25,5 +78,29 @@ export class RequestEntity {
         this.active = false;
         this.createdAt = new Date();
         this.updatedAt = new Date();
+        this.message = data.message;
+        this.serviceId = data.serviceId;
+        this.ownerId = data.ownerId;
+        this.customerId = data.customerId;
+        this.customerUserId = data.customerUserId;
+        this.professionalId = data.professionalId;
+        this.status = 0;
+        this.createdForId = data.createdForId;
+        this.read = false;
+        this.push = data.push;
+        this.email = data.email;
+        this.hasDelivery = data.hasDelivery;
+        this.hasRecurrence = data.hasRecurrence;
+        this.hasFidelity = data.hasFidelity;
+        this.hasRide = data.hasRide;
+        this.initDate = data.initDate;
+        this.endDate = data.endDate;
+        this.cancelledAt = data.cancelledAt;
+        this.fidelity = data.fidelity;
+        this.recurrence = data.recurrence;
+        this.order = data.order;
+        this.ride = data.ride;
+        this.updatedById = null;
+        this.updatedByRole = null;
     }
 }
